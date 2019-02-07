62f6aad43e9902b1508ab9ba165fd2017131c052
Tristan <tristan.bessac@live.fr>
2019-02-07 16:50:32 +0100

    structure du projet

---
+++ src/ReedSolomon/encryptRS.c
@@ -0,0 +1,19 @@
+#include "../../inc/ReedSolomon/encryptRS.h"
+#include <flint.h>
+#include <stdlib.h>
+
+void generateG(fmpz_mod_poly_t res){
+
+}
+
+void findB(fmpz_mod_poly_t res,fmpz_mod_poly_t Generator){
+
+}
+
+
+
+
+
+main(int argc, char** argv){
+
+}
